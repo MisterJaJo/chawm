7e6fa1a886ab3419c8de4998dea28dfc4b2dc341
MisterJaJo <jarvistrigo@gmail.com>
2022-04-01 13:54:48 +0200

    Added root window event mask setup

--- src/instance.c
+++ src/instance.c
@@ -15,6 +15,18 @@ struct chawm_instance *chawm_instance_new()
 	const xcb_setup_t *setup = xcb_get_setup(inst->conn);
 	inst->screen = xcb_setup_roots_iterator(setup).data;
 
+	// Set screen root window event mask
+	unsigned int event_mask[1] =
+	{
+		  XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT
+		| XCB_EVENT_MASK_STRUCTURE_NOTIFY
+		| XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
+		| XCB_EVENT_MASK_PROPERTY_CHANGE
+	};
+	xcb_change_window_attributes_checked(
+			inst->conn, inst->screen->root, 
+			XCB_CW_EVENT_MASK, event_mask);
+
 	return inst;
 }
 
