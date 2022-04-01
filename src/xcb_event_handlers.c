3ee6f33811f3a1c72c024547de5a08a4d6ef33de
MisterJaJo <jarvistrigo@gmail.com>
2022-04-01 17:55:11 +0200

    Added definitions for remaining xcb events

--- src/xcb_event_handlers.c
+++ src/xcb_event_handlers.c
@@ -1,6 +1,30 @@
 #include "xcb_event_handlers.h"
 
-void chawm_handle_keypress(struct chawm_instance *inst, xcb_generic_event_t *event)
+EV_HANDLER_DEF(motion_notify)
+{
+}
+
+EV_HANDLER_DEF(enter_notify)
+{
+}
+
+EV_HANDLER_DEF(destroy_notify)
+{
+}
+
+EV_HANDLER_DEF(map_request)
+{
+}
+
+EV_HANDLER_DEF(focus_in)
+{
+}
+
+EV_HANDLER_DEF(focus_out)
+{
+}
+
+EV_HANDLER_DEF(key_press)
 {
 	xcb_key_press_event_t *kp_event = (xcb_key_press_event_t *) event;
 	xcb_keysym_t keysym = chawm_instance_get_keysym(inst, kp_event->detail);
@@ -15,19 +39,21 @@ void chawm_handle_keypress(struct chawm_instance *inst, xcb_generic_event_t *eve
 	}
 }
 
-void chawm_handle_buttonpress(struct chawm_instance *inst, xcb_generic_event_t *event)
+EV_HANDLER_DEF(button_press)
 {
 	xcb_button_press_event_t *bp_event = (xcb_button_press_event_t *) event;
-	
-	// TODO: window dragging
+	xcb_button_t button = bp_event->detail;
 
-	xcb_grab_pointer(inst->conn, 0, inst->screen->root,
-			   XCB_EVENT_MASK_BUTTON_RELEASE 
-			 | XCB_EVENT_MASK_BUTTON_MOTION 
-			 | XCB_EVENT_MASK_POINTER_MOTION_HINT,
-			 XCB_GRAB_MODE_ASYNC,
-			 XCB_GRAB_MODE_ASYNC,
-			 inst->screen->root,
-			 XCB_NONE,
-			 XCB_CURRENT_TIME);
+	for (int i = 0; i < inst->button_actions_count; ++i)
+	{
+		struct chawm_button_action button_action = inst->button_actions[i];
+		if (button_action.button == button && button_action.modifier == bp_event->state)
+		{
+			chawm_instance_handle_instance_event(inst, button_action.event);
+		}
+	}
+}
+
+EV_HANDLER_DEF(button_release)
+{
 }
--- src/xcb_event_handlers.h
+++ src/xcb_event_handlers.h
@@ -5,14 +5,36 @@
 #include "util.h"
 #include "instance.h"
 
-void chawm_handle_keypress(struct chawm_instance *inst, xcb_generic_event_t *event);
-void chawm_handle_buttonpress(struct chawm_instance *inst, xcb_generic_event_t *event);
+#define EV_HANDLER(evname) chawm_handle_##evname
+#define EV_HANDLER_DEF(evname) \
+	void EV_HANDLER(evname)(struct chawm_instance *inst, xcb_generic_event_t *event)
+
+EV_HANDLER_DEF(motion_notify);
+EV_HANDLER_DEF(enter_notify);
+EV_HANDLER_DEF(destroy_notify);
+EV_HANDLER_DEF(map_request);
+EV_HANDLER_DEF(focus_in);
+EV_HANDLER_DEF(focus_out);
+EV_HANDLER_DEF(key_press);
+EV_HANDLER_DEF(button_press);
+EV_HANDLER_DEF(button_release);
 
 static struct chawm_instance_xcb_event_handler CHAWM_XCB_EVENT_HANDLERS[] =
 {
-	{ XCB_KEY_PRESS,    chawm_handle_keypress },
-	{ XCB_BUTTON_PRESS, chawm_handle_buttonpress },
-	{ XCB_NONE,         NULL },
+	{ XCB_MOTION_NOTIFY,  EV_HANDLER(motion_notify)  },
+	{ XCB_ENTER_NOTIFY,   EV_HANDLER(enter_notify)   },
+	{ XCB_DESTROY_NOTIFY, EV_HANDLER(destroy_notify) },
+
+	{ XCB_MAP_REQUEST,    EV_HANDLER(map_request) },
+
+	{ XCB_FOCUS_IN,       EV_HANDLER(focus_in)  },
+	{ XCB_FOCUS_OUT,      EV_HANDLER(focus_out) },
+
+	{ XCB_KEY_PRESS,      EV_HANDLER(key_press)      },
+	{ XCB_BUTTON_PRESS,   EV_HANDLER(button_press)   },
+	{ XCB_BUTTON_RELEASE, EV_HANDLER(button_release) },
+
+	{ XCB_NONE,           NULL },
 };
 
 static unsigned int CHAWM_XCB_EVENT_HANDLERS_COUNT = LENGTH(CHAWM_XCB_EVENT_HANDLERS);
